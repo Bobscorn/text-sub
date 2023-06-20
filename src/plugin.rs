6543d6b2c2132651be0aba06a855db55af186d22
Bobscorn <qwertster0@gmail.com>
2023-06-20 13:02:16 +1200

    disable print_position_system

--- src/plugin.rs
+++ src/plugin.rs
@@ -9,7 +9,6 @@ impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app
             .add_event::<TorpedoCollisionEvent>()
-            .add_system(print_position_system)
             .add_startup_system(hello_world)
             .add_startup_system(setup_world);
     }
