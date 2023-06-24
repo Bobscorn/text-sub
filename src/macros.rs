1dbf3eabd05b6c7eca08cb620e8b5565bdf13ff9
sam <mail@samueljenks.me>
2023-06-25 09:57:02 +1200

    chore: enable log() function for wasm

---
+++ src/macros.rs
@@ -0,0 +1,8 @@
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
